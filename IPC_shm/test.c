012b33bbe0021c0ff0eac41a9c6b417710ca1f2b
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-07-09 18:21:30 +0000

    refactor: organize code in main to enhance readability

--- IPC_shm/test.c
+++ IPC_shm/test.c
@@ -7,64 +7,97 @@
 
 #include "shm_queue.h"
 
-#define SHM_NAME "/ipc_14"
+#define SHM_NAME "/ipc"
 #define CAPACITY 1000
 #define MESSAGES_N 10000000
+
+void print_time(const char *msg);
+
+int run_parent(void);
+int run_child(void);
+
+int main(void)
+{
+    int ret = 0;
+
+    switch (fork())
+    {
+    case -1:
+        perror("fork");
+        ret = 1;
+        break;
+
+    case 0:
+        ret = run_parent();
+
+        wait(NULL);
+        break;
+
+    default:
+        ret = run_child();
+        break;
+    }
+
+    return ret;
+}
+
+int run_parent(void)
+{
+    if (shm_queue_create(sizeof(uint32_t), CAPACITY, SHM_NAME, &shm_queue) != 0)
+    {
+        perror("shm_queue_create_call");
+        return 1;
+    }
+
+    print_time("start");
+
+    for (uint32_t i = 0; i < MESSAGES_N; i++)
+        if (shm_queue_push(shm_queue, (void *)&i) != 0)
+        {
+            perror("shm_queue_push_call");
+            break;
+        }
+
+    if (shm_queue_close(shm_queue) != 0)
+    {
+        perror("shm_queue_close");
+        return 1;
+    }
+
+    return 0;
+}
+
+int run_child(void)
+{
+    shm_queue_t *shm_queue = NULL;
+    if (shm_queue_open(SHM_NAME, &shm_queue))
+    {
+        printf("couldn't open queue\n");
+        return 0;
+    }
+
+    char buf[sizeof(uint32_t)];
+    for (uint32_t i = 0; i < MESSAGES_N; i++)
+        if (shm_queue_pop(shm_queue, buf) != 0)
+        {
+            perror("shm_queue_pop_call");
+            break;
+        }
+
+    print_time("finish");
+
+    if (shm_queue_close(shm_queue) != 0)
+    {
+        perror("shm_queue_close_call");
+        return 1;
+    }
+
+    return 0;
+}
 
 void print_time(const char *msg)
 {
     struct timeval time;
     gettimeofday(&time, NULL);
-    printf("%sed at %lu us\n", msg, ((time.tv_sec) * 1000000 + time.tv_usec) % 10000000);
-}
-
-int main(void)
-{
-    struct timeval stop, start;
-    int err_code;
-    shm_queue_t *shm_queue = NULL;
-
-    pid_t pid = fork();
-
-    switch (pid)
-    {
-    case -1: // error
-        perror("fork");
-        break;
-
-    case 0:
-    { // parent
-        assert(shm_queue_create(sizeof(uint32_t), CAPACITY, SHM_NAME, &shm_queue) == 0);
-
-        print_time("start");
-
-        for (uint32_t i = 0; i < MESSAGES_N; i++)
-            assert(shm_queue_push(shm_queue, (void *)&i) == 0);
-
-        assert(shm_queue_close(shm_queue) == 0);
-
-        wait(NULL);
-        break;
-    }
-
-    default:
-    { // child
-        if (shm_queue_open(SHM_NAME, &shm_queue))
-        {
-            printf("couldn't open queue\n");
-            return 0;
-        }
-
-        char buf[sizeof(uint32_t)];
-        for (uint32_t i = 0; i < MESSAGES_N; i++)
-            assert(shm_queue_pop(shm_queue, buf) == 0);
-
-        print_time("finish");
-
-        assert(shm_queue_close(shm_queue) == 0);
-        break;
-    }
-    }
-
-    return 0;
+    printf("%sed at \t%lu us\n", msg, ((time.tv_sec) * 1000000 + time.tv_usec) % 10000000);
 }

c11c99228f7546f08d7100ee3beedeed0844e508
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-07-09 16:34:31 +0000

    refactor: organize code for server/client run

--- IPC_socket/test.c
+++ IPC_socket/test.c
@@ -8,7 +8,7 @@
 
 #include "socket_pipe.h"
 
-#define SOCKET_FILE_NAME "/tmp/somepath10"
+#define SOCKET_FILE_NAME "/tmp/somepath"
 
 #define MESSAGE_N 10000000
 
@@ -16,16 +16,15 @@ void print_time(const char *msg)
 {
     struct timeval time;
     gettimeofday(&time, NULL);
-    printf("%sed at %lu us\n", msg, ((time.tv_sec) * 1000000 + time.tv_usec) % 10000000);
+    printf("%sed at \t%lu us\n", msg, ((time.tv_sec) * 1000000 + time.tv_usec) % 10000000);
 }
+
+int run_server(void);
+int run_client(void);
 
 int main(void)
 {
-    pid_t pid = fork();
-
-    socket_pipe_t *socket_pipe = NULL;
-
-    switch (pid)
+    switch (fork())
     {
     case -1:
         perror("fork");
@@ -33,61 +32,81 @@ int main(void)
 
     case 0: // parent
     {
-        if (socket_pipe_create(sizeof(uint32_t), SOCKET_FILE_NAME, &socket_pipe) != 0)
-        {
-            perror("socket_create_call");
-            return 1;
-        }
-
-        print_time("start");
-
-        for (uint32_t i = 0; i < MESSAGE_N; i++)
-            if (socket_pipe_send(socket_pipe, (void *)&i) != 0)
-            {
-                perror("socket_pipe_send_call");
-                break;
-            }
+        run_server();
 
         wait(NULL);
-        if (socket_pipe_destroy(socket_pipe) != 0)
-        {
-            perror("socket_pipe_destroy_call");
-            return 1;
-        }
 
         break;
     }
     default: // child
     {
-        uint32_t data = 0;
-        if (socket_pipe_open(SOCKET_FILE_NAME, &socket_pipe) != 0)
-        {
-            perror("socket_pipe_open_call");
-            return 1;
-        }
+        run_client();
 
-        for (uint32_t i = 0; i < MESSAGE_N; i++)
-        {
-            if (socket_pipe_recv(socket_pipe, (void *)&data) != 0)
-            {
-                perror("socket_pipe_recv_call");
-                break;
-            }
-
-            // printf("%u: GOT DATA %u\n", i, data);
-        }
-
-        print_time("end");
-
-        if (socket_pipe_close(socket_pipe) != 0)
-        {
-            perror("socket_pipe_close_call");
-            return 1;
-        }
 
         break;
     }
     }
 
     return 0;
+}
+
+int run_server(void)
+{
+    socket_pipe_t *socket_pipe = NULL;
+
+    if (socket_pipe_create(sizeof(uint32_t), SOCKET_FILE_NAME, &socket_pipe) != 0)
+    {
+        perror("socket_create_call");
+        return 1;
+    }
+
+    print_time("start");
+
+    for (uint32_t i = 0; i < MESSAGE_N; i++)
+    {
+        if (socket_pipe_send(socket_pipe, (void *)&i) != 0)
+        {
+            perror("socket_pipe_send_call");
+            return 1;
+        }
+    }
+
+    if (socket_pipe_destroy(socket_pipe) != 0)
+    {
+        perror("socket_pipe_destroy_call");
+        return 1;
+    }
+
+    return 0;
+}
+
+int run_client(void)
+{
+    socket_pipe_t *socket_pipe = NULL;
+
+    if (socket_pipe_open(SOCKET_FILE_NAME, &socket_pipe) != 0)
+    {
+        perror("socket_pipe_open_call");
+        return 1;
+    }
+
+    uint32_t data = 0;
+    for (uint32_t i = 0; i < MESSAGE_N; i++)
+    {
+        if (socket_pipe_recv(socket_pipe, (void *)&data) != 0)
+        {
+            perror("socket_pipe_recv_call");
+            return 1;
+        }
+
+        // printf("%u: GOT DATA %u\n", i, data);
+    }
+
+    print_time("end");
+
+    if (socket_pipe_close(socket_pipe) != 0)
+    {
+        perror("socket_pipe_close_call");
+        return 1;
+    }
 }

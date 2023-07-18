3e0148a1a582a6cbe67c9e2abc6ee72c46e7edcc
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-07-18 10:08:31 +0000

    refactor: Make timing of main loop independent of initialization and teardown

--- IPC_socket/benchmark/socket_benchmark.cpp
+++ IPC_socket/benchmark/socket_benchmark.cpp
@@ -1,146 +1,142 @@
-/*#include <sys/time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <assert.h>
-#include <stdint.h>
-#include <sys/wait.h>
-
-#include "socket_pipe.h"
-
-#define SOCKET_FILE_NAME "/tmp/somepath"
-
-#define MESSAGE_N 10000000
-
-void print_time(const char *msg)
-{
-    struct timeval time;
-    gettimeofday(&time, NULL);
-    printf("%sed at \t%lu us\n", msg, ((time.tv_sec) * 1000000 + time.tv_usec) % 10000000);
-}
-
-int run_server(void);
-int run_client(void);
-
-int main(void)
-{
-    int ret = 0;
-
-    switch (fork())
-    {
-    case -1:
-        perror("fork");
-        ret = 1;
-        break;
-
-    case 0:
-        ret = run_server();
-
-        wait(NULL);
-        break;
-
-    default:
-        ret = run_client();
-        break;
-    }
-
-    return ret;
-}
-
-int run_server(void)
-{
-    socket_pipe_t *socket_pipe = NULL;
-
-    if (socket_pipe_create(sizeof(uint32_t), SOCKET_FILE_NAME, &socket_pipe) != 0)
-    {
-        perror("socket_create_call");
-        return 1;
-    }
-
-    print_time("start");
-
-    for (uint32_t i = 0; i < MESSAGE_N; i++)
-        if ((socket_pipe_send(socket_pipe, (void *)&i)) != 0)
-        {
-            perror("socket_pipe_send_call");
-            break;
-        }
-
-    if (socket_pipe_destroy(socket_pipe) != 0)
-    {
-        perror("socket_pipe_destroy_call");
-        return 1;
-    }
-
-    return 0;
-}
-
-int run_client(void)
-{
-    socket_pipe_t *socket_pipe = NULL;
-
-    if (socket_pipe_open(SOCKET_FILE_NAME, &socket_pipe) != 0)
-    {
-        perror("socket_pipe_open_call");
-        return 1;
-    }
-
-    uint32_t data = 0;
-    for (uint32_t i = 0; i < MESSAGE_N; i++)
-    {
-        if (socket_pipe_recv(socket_pipe, (void *)&data) != 0)
-        {
-            perror("socket_pipe_recv_call");
-            break;
-        }
-
-        // printf("%u: GOT DATA %u\n", i, data);
-    }
-
-    print_time("end");
-
-    if (socket_pipe_close(socket_pipe) != 0)
-    {
-        perror("socket_pipe_close_call");
-        return 1;
-    }
-}*/
-
 extern "C"
 {
 #include "../socket_pipe.h"
 }
 
 #include <benchmark/benchmark.h>
-#include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <thread>
+#include <sys/eventfd.h>
 
-void socket_simulate(int cnt)
-{
-    while (cnt--)
-        sleep(1);
-}
+#define EXIT_ON_ERROR(val) \
+    do                     \
+    {                      \
+        if (val != 0)      \
+        {                  \
+            perror(#val);  \
+            exit(1);       \
+        }                  \
+    } while (0)
+
+#define RETURN_ON_ERROR(val) \
+    do                       \
+    {                        \
+        if (val != 0)        \
+        {                    \
+            perror(#val);    \
+            return -1;       \
+        }                    \
+    } while (0)
+
+#define SOCKET_FILE_NAME "/tmp/somepath"
+
+static int pop_many_elements(socket_pipe_t *socket_pipe, size_t n);
+static int push_many_elements(socket_pipe_t *socket_pipe, size_t n);
+
+static void Bench_socket_simulate(benchmark::State &state);
+
+BENCHMARK(Bench_socket_simulate)->RangeMultiplier(2)->Range(1, 10000000);
+
+BENCHMARK_MAIN();
 
 static void Bench_socket_simulate(benchmark::State &state)
 {
+    // set up
+    eventfd_t eventfd_val = 10;
+    int start_fd, end_fd;
+    pid_t pid = -1;
+    socket_pipe_t *socket_pipe_client;
+    socket_pipe_t *socket_pipe_server;
+    auto thread_id = std::thread([&]()
+                                 { EXIT_ON_ERROR(socket_pipe_open(SOCKET_FILE_NAME, &socket_pipe_client)); });
+
+    EXIT_ON_ERROR(socket_pipe_create(sizeof(uint32_t), SOCKET_FILE_NAME, &socket_pipe_server));
+
+    thread_id.join();
+
+    start_fd = eventfd(0, 0);
+    end_fd = eventfd(0, 0);
+
+    if (start_fd < 0 || end_fd < 0)
+        exit(1);
+
+    pid = fork();
+
+    // main loop
     for (auto _ : state)
     {
-        pid_t pid = fork();
+        switch (pid)
+        {
+        case -1:
+            perror("fork");
+            exit(1);
+            break;
 
-        if (pid == 0)
+        case 0:
         {
-            std::cout << "I'm parent" << std::endl;
-            sleep(1);
+            eventfd_read(start_fd, &eventfd_val);
+
+            EXIT_ON_ERROR(pop_many_elements(socket_pipe_client, state.range(0)));
+
+            eventfd_write(end_fd, eventfd_val);
+            break;
         }
-        else
+        default:
         {
-            std::cout << "I'm child" << std::endl;
-            sleep(1);
+            eventfd_write(start_fd, eventfd_val);
+
+            EXIT_ON_ERROR(push_many_elements(socket_pipe_server, state.range(0)));
+
+            eventfd_read(end_fd, &eventfd_val);
+            break;
         }
+        }
+    }
+
+    // tear down
+    if (pid == 0)
+    {
+        EXIT_ON_ERROR(socket_pipe_close(socket_pipe_client));
+        exit(0);
+    }
+    else
+    {
+        EXIT_ON_ERROR(socket_pipe_destroy(socket_pipe_server));
+        wait(NULL);
     }
 }
 
-BENCHMARK(Bench_socket_simulate); //->RangeMultiplier(2)->Range(1, 10);
+static int pop_many_elements(socket_pipe_t *socket_pipe, size_t n)
+{
+    uint32_t data = 0;
+    for (uint32_t i = 0; i < n; i++)
+    {
+        RETURN_ON_ERROR(socket_pipe_recv(socket_pipe, (void *)&data));
 
-BENCHMARK_MAIN();
+        // printf("%u: GOT DATA %u\n", i, data);
+    }
+
+    return 0;
+}
+
+static int pop_many_elements(socket_pipe_t *socket_pipe, size_t n)
+{
+    uint32_t data = 0;
+    for (uint32_t i = 0; i < n; i++)
+    {
+        RETURN_ON_ERROR(socket_pipe_recv(socket_pipe, (void *)&data));
+
+        // printf("%u: GOT DATA %u\n", i, data);
+    }
+
+    return 0;
+}
+
+static int push_many_elements(socket_pipe_t *socket_pipe, size_t n)
+{
+    for (uint32_t i = 0; i < n; i++)
+        RETURN_ON_ERROR(socket_pipe_send(socket_pipe, (void *)&i));
+
+    return 0;
+}
